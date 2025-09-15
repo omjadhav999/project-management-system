02d1144f2bf6ae44bd98db3cd0548f9c87afc17d
Om Jadhav <jadhavom229@gmail.com>
2025-09-15 23:29:27 +0530

    Fix root route: remove error throw, keep success response

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,8 @@ import connectDatabase from "./config/database.config";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
 import { HTTPSTATUS } from "./config/http.config";
 import { asyncHandler } from "./middlewares/asyncHandler.middleware";
-import { BadRequestException } from "./utils/appError";
-import { ErrorCodeEnum } from "./enums/error-code.enum";
-
+// import { BadRequestException } from "./utils/appError";
+// import { ErrorCodeEnum } from "./enums/error-code.enum";
 import "./config/passport.config";
 import passport from "passport";
 import authRoutes from "./routes/auth.route";
@@ -49,13 +48,22 @@ app.use(
   })
 );
 
+// app.get(
+//   /,
+//   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+//     throw new BadRequestException(
+//       "This is a bad request",
+//       ErrorCodeEnum.AUTH_INVALID_TOKEN
+//     );
+//     return res.status(HTTPSTATUS.OK).json({
+//       message: "Hello Subscribe to the channel & share",
+//     });
+//   })
+// );
+
 app.get(
   `/`,
   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    throw new BadRequestException(
-      "This is a bad request",
-      ErrorCodeEnum.AUTH_INVALID_TOKEN
-    );
     return res.status(HTTPSTATUS.OK).json({
       message: "Hello Subscribe to the channel & share",
     });
